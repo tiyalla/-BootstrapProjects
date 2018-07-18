fe438d309e231e443bfae4848d562cefba58ca42
Tonye Iyalla <tiyalla2015@gmail.com>
2018-07-18 07:32:16 -0400

    Fix image error

---
+++ FashionPortfolio/js/main.js
@@ -0,0 +1,6 @@
+$( document ).ready(function() {
+    // $('.navbar-toggler-icon').click(function() {
+    //     $('.navList li a').addClass('nav-style');
+    //  $('.navList ul').toggleClass('nav-open');
+    // });
+});
