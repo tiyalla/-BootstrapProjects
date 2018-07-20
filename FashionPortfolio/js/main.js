e52f0168e96f3d92474917453129906753b157b5
Iyalla <iyalla@theinstitutes.org>
2018-07-20 15:59:37 -0400

    Create about us html --> added content and styling for this page

--- FashionPortfolio/js/main.js
+++
@@ -1,6 +0,0 @@
-$( document ).ready(function() {
-    // $('.navbar-toggler-icon').click(function() {
-    //     $('.navList li a').addClass('nav-style');
-    //  $('.navList ul').toggleClass('nav-open');
-    // });
-});
